f1e8759aa9960c9e92bb73889b09cc7b43d177c4
elizeurs <13ders@gmail.com>
2019-01-17 18:22:36 -0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Elizeu RS on 16/01/19.
+//  Copyright © 2019 elizeurs. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
+
